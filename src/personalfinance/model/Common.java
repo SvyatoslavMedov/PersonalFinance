23a781a34abc317293a8ef2b4c9774587d02324f
SvyatoslavMedov <irobot1204@gmail.com>
2021-06-26 20:34:17 +0600

    Initial commit

---
+++ src/personalfinance/model/Common.java
@@ -0,0 +1,14 @@
+package personalfinance.model;
+
+abstract public class Common {
+
+    public Common() {}
+
+    public String getValueForComboBox() {
+        return null;
+    }
+
+    public void postAdd() {}
+    public void postEdit() {}
+    public void postRemove() {}
+}
